88ef00deacf33a4ccc2c4321b216c2cb3f7da743
Suncerie Daye <sunceried@gmail.com>
2021-09-13 10:19:03 -0700

    initial setup

---
+++ src/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+module.exports = app;
