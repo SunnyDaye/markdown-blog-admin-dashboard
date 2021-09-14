b5b6c63a267834bc3c097142f1295412199eda1e
Suncerie Daye <sunceried@gmail.com>
2021-09-14 13:17:19 -0700

    database connection configured

--- src/db/migrations/20210913213200_createArticlesTable.js
+++ src/db/migrations/20210913213200_createArticlesTable.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('articles', (table) => {
+    table.increments('articleId').primary();
+    table.string('title').notNullable();
+    table.text('description').notNullable();
+    table.text('markdown').notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('articles');
 };
